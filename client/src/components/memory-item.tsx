0baa4c7b5e4539b86345ba2b1a7811b6735af52d
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2024-10-01 01:09:08 +0530

    Updated Memory Routes

--- client/src/components/memory-item.tsx
+++ client/src/components/memory-item.tsx
@@ -2,17 +2,28 @@ import { Badge } from "@/components/ui/badge";
 import { MemoryItemInterface } from "@/types/memory-types";
 import { CiClock1 } from "react-icons/ci";
 import { DateTime } from "luxon";
+import { useState } from "react";
+import MemoryModal from "./memory-modal";
 
 const MemoryItem = (props: { item: MemoryItemInterface }) => {
+  const [showModal, setShowModal] = useState(false);
+
   function convertToCustomFormat(dateString: string): string {
     const parsedDate = DateTime.fromISO(dateString);
     const formattedDate = parsedDate.toFormat("dd/MM/yyyy, HH:mm:ss");
     return formattedDate;
   }
 
+  const handleShowModal = (value: boolean) => {
+    setShowModal(value);
+  };
+
   return (
     <>
-      <div className="bg-gray-50 p-3 border-b">
+      {
+        showModal&&<MemoryModal handleShowModal={handleShowModal} item={props.item} />
+      }
+      <div onClick={()=>handleShowModal(true)} className="bg-gray-50 p-3 border-b">
         <h4 className="font-medium my-1">{props.item.memory}</h4>
         <p className="text-sm text-gray-400 flex gap-1 align-middle my-1">
           <CiClock1 className="text-gray-600 my-auto" />{" "}
--- client/src/context/MemoryContext.tsx
+++ client/src/context/MemoryContext.tsx
@@ -4,14 +4,14 @@ const MemoryContext = createContext<any>({});
 const url = import.meta.env.VITE_URL;
 
 const MemoryState = (props: any) => {
-    const [memories, setMemories] = useState([]);
+  const [memories, setMemories] = useState([]);
+
   const getMemories = async (userid: string) => {
     try {
       const response = await fetch(`${url}/api/memory?user_id=${userid}`);
       const data = await response.json();
 
       if (data.success) {
-        console.log(data.memories);
         setMemories(data.memories);
         return true;
       } else {
@@ -25,8 +25,52 @@ const MemoryState = (props: any) => {
     }
   };
 
+  const updateMemory = async (memoryid: string, text: string) => {
+    try {
+      const response = await fetch(`${url}/api/memory`, {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          memoryid: memoryid,
+          text: text,
+        }),
+      });
+      const data = await response.json();
+
+      if (data.success) {
+        await getMemories(data.memories.user_id);
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  };
+
+  const deleteMemory = async (memoryid: string) => {
+    try {
+      const response = await fetch(`${url}/api/memory?memoryid=${memoryid}`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await response.json();
+
+      if (data.success) {
+        await getMemories(data.memories.user_id);
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  };
+
   return (
-    <MemoryContext.Provider value={{ getMemories, memories }}>
+    <MemoryContext.Provider value={{ getMemories, memories, updateMemory, deleteMemory }}>
       {props.children}
     </MemoryContext.Provider>
   );
